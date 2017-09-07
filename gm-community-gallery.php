dc7774f0af565ee57d5a3e7a6af420c7fa83e2ce
GabrielMioni <gabriel.mioni@gmail.com>
2017-09-06 18:33:38 -0700

    Added admin features.

--- gm-community-gallery.php
+++ gm-community-gallery.php
@@ -6,7 +6,7 @@ error_reporting(-1);
 Plugin Name: GM Community Gallery
 Version: 1.0
 Plugin URI: http://gabrielmioni.com/gm-community-gallery
-Description: Automated accept image files and add them to a WordPress gallery
+Description: Automated accept image files and add them to a WordPress submit
 Author: Gabriel Mioni
 Author URI: http://gabrielmioni.com
 
@@ -33,7 +33,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 define( 'GM_GALLERY_VERSION', '1.0' );
 
+define( 'GM_GALLERY_DIR', plugin_dir_path(__FILE__) );
+
 define( 'GM_GALLERY_TABLENAME', gm_gallery_define_tablename());
+
 function gm_gallery_define_tablename()
 {
     global $wpdb;
@@ -56,19 +59,19 @@ function gm_gallery_define_tablename()
 register_activation_hook( __FILE__, 'gm_community_gallery_activated' );
 function gm_community_gallery_activated()
 {
-    gm_gallery_create_directories();
     gm_gallery_create_sql_db();
+    gm_gallery_create_directories();
 }
 
 /* ******************************************************
- * - Create the /uploads/gm-community-gallery directory
+ * - Create the /uploads/gm-community-submit directory
  * ******************************************************/
 
 function gm_gallery_create_directories()
 {
     $wp_uploads_dir = wp_upload_dir();
     $wp_uploads_dir_base = $wp_uploads_dir['basedir'];
-    $gm_directory = $wp_uploads_dir_base . '/gm-community-gallery';
+    $gm_directory = $wp_uploads_dir_base . '/gm-community-submit';
     $gm_thumbs = $gm_directory . '/thumbs';
     $gm_images = $gm_directory . '/images';
 
@@ -85,15 +88,20 @@ function gm_gallery_create_directories()
 function gm_gallery_create_sql_db()
 {
     $tablename = GM_GALLERY_TABLENAME;
+
     $create_table =
         "CREATE TABLE $tablename (
-            id CHAR(6) PRIMARY KEY NOT NULL,
-            type CHAR(4),
-            email VARCHAR(100),
-            ip VARCHAR(10),
-            name VARCHAR(100),
-            title VARCHAR(200),
-            message VARCHAR(600)
+            `id` CHAR(6) PRIMARY KEY NOT NULL,
+            `type` CHAR(4),
+            `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            `email` VARCHAR(100),
+            `ip` VARCHAR(10),
+            `name` VARCHAR(100),
+            `title` VARCHAR(200),
+            `message` VARCHAR(600),
+            `comment` VARCHAR(600),
+            `tags` VARCHAR(600),
+            `trash` TINYINT(1) NOT NULL DEFAULT 0
         );";
 
     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
@@ -107,20 +115,20 @@ function gm_gallery_create_sql_db()
 add_action( 'wp_enqueue_scripts', 'gm_register_gallery_css' );
 function gm_register_gallery_css()
 {
-    wp_register_style( 'gm-gallery-css', plugins_url( '/css/gm-gallery.css', __FILE__ ), array(), GM_GALLERY_VERSION, 'all' );
+    wp_register_style( 'gm-submit-css', plugins_url( '/css/gm-submit.css', __FILE__ ), array(), GM_GALLERY_VERSION, 'all' );
 }
 
 /* *******************************
  * - Contact Submit Form Shortcode
  * *******************************/
 
-add_shortcode('gm-gallery-form', 'gm_gallery_form_shortcode');
+add_shortcode('gm-submit-form', 'gm_gallery_form_shortcode');
 function gm_gallery_form_shortcode()
 {
-    require_once('gm-community-gallery-html.php');
+    require_once('submit/php/class.image_upload_form.php');
 
     // Add the gm-contact.css file
-    wp_enqueue_style('gm-gallery-css');
+    wp_enqueue_style('gm-submit-css');
 
     $auto_p_flag = false;
 
@@ -132,7 +140,7 @@ function gm_gallery_form_shortcode()
         remove_filter( 'the_excerpt', 'wpautop' );
     }
 
-    $build_html = new gm_community_gallery_html();
+    $build_html = new GM_community_gallery\submit\image_upload_form();
     echo $build_html->return_html();
 
     // If wpautop had been set previously, re-enable it.
@@ -141,6 +149,51 @@ function gm_gallery_form_shortcode()
         add_filter( 'the_content', 'wpautop' );
         add_filter( 'the_excerpt', 'wpautop' );
     }
+}
+
+/* *******************************
+ * - Settings Page
+ * *******************************/
+
+add_action( 'wp_enqueue_scripts', 'gm_register_gallery_css' );
+function gm_register_settings_css()
+{
+    wp_enqueue_style( 'gm-gallery-settings-css', plugins_url( 'admin/css/gm-gallery-settings.css', __FILE__ ), array(), GM_GALLERY_VERSION, 'all' );
+}
+
+add_action( 'admin_menu', 'gm_community_gallery_menu' );
+function gm_community_gallery_menu()
+{
+    $menu = add_menu_page( 'GM Community Gallery', 'GM Community Gallery', 'manage_options', 'gm-community-submit', 'gm_community_options' );
+
+    add_action( 'admin_print_styles-' . $menu, 'gm_register_settings_css' );
+}
+
+function gm_community_options()
+{
+    if ( !current_user_can( 'manage_options' ) )
+    {
+        wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+    }
+
+    echo '<div class="wrap">';
+
+    if ( isset( $_GET['edit'] ) )
+    {
+        require_once('admin/php/class.image_update_form.php');
+
+        $build_form = new GM_community_gallery\admin\build_image_edit_form();
+        echo $build_form->return_html_form();
+
+    } else {
+
+        require_once('admin/php/class.admin_gallery_build.php');
+
+        $build_admin_gallery = new GM_community_gallery\admin\admin_gallery_build();
+        echo $build_admin_gallery->return_admin_gallery_html();
+    }
+
+    echo '</div>';
 }
 
 /* *******************************
@@ -153,7 +206,23 @@ function gm_gallery_check_api()
 
     if ($api_set === true)
     {
-        require_once(dirname(__FILE__) . '/gm-community-gallery-submit.php');
-        new gm_community_gallery_submit();
+        require_once('submit/php/class.image_upload_process.php');
+
+        new GM_community_gallery\submit\image_upload_process();
+
+    }
+}
+
+add_action('init', 'gm_update_admin');
+function gm_update_admin()
+{
+    $api_set = isset($_GET['gm_community_admin']) ? true : false;
+
+    if ($api_set === true)
+    {
+        require_once('admin/php/class.image_update_process.php');
+
+        new GM_community_gallery\admin\image_update_process();
+
     }
 }
