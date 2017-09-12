789742b58c120d740a9ca757a96df97ceec73dd8
GabrielMioni <gabriel.mioni@gmail.com>
2017-09-12 16:55:37 -0700

    Added documentation.

--- admin/php/class.image_update_process.php
+++ admin/php/class.image_update_process.php
@@ -1,14 +1,33 @@
 <?php
 
+/**
+ * @package     GM-Community-Gallery
+ * @author      Gabriel Mioni <gabriel@gabrielmioni.com>
+ */
+
 namespace GM_community_gallery\admin;
 
+/**
+ * Processes the submitted update from the image edit form found at wp-admin/admin.php?page=gm-community-gallery&edit=[id]
+ * The row for the associated image in the gm_community_gallery MySQL table will be updated.
+ *
+ * This class also sets a response message in $_SESSION['response']
+ *
+ * @package GM_community_gallery\admin
+ * @see image_update_form
+ */
 class image_update_process
 {
+    /** @var bool TRUE if nonce was validate, else false */
     protected $nonce_ok;
 
+    /** @var string The image id set at $_GET['edit'] */
     protected $image_id;
+
+    /** @var array Holds input values from the image edit form */
     protected $input_array = array();
 
+    /** @var false|int -1 if image was moved to trash. 1 if record was updated, false if not */
     protected $sql_update_flag;
 
     public function __construct()
@@ -28,18 +47,17 @@ class image_update_process
 
         $this->sql_update_flag = $this->update_gm_community_gallery_table($this->input_array, $this->image_id);
 
-        /*  This is used for testing
-        foreach ($_POST as $key=>$value)
-        {
-            $_SESSION[$key] = $value;
-        }
-        */
-
         $this->set_response_message($this->sql_update_flag);
 
         $this->do_redirect();
     }
 
+    /**
+     * Validate the nonce set class.admin_update_form.php
+     *
+     * @return bool     True if nonce is valid. Else false.
+     * @see image_update_form::build_edit_form()
+     */
     protected function validate_nonce()
     {
         $nonce = $_POST['_wpnonce'];
@@ -53,6 +71,12 @@ class image_update_process
         die('You have now power here.');
     }
 
+    /**
+     * Collects input values.
+     *
+     * @param   $index  string  Checks for values at $_POST[$index]
+     * @return          string  If no value is found, return whitespace. Else, return value with tags stripped.
+     */
     protected function set_input($index)
     {
         if (isset($_POST[$index]))
@@ -63,6 +87,13 @@ class image_update_process
         return '';
     }
 
+    /**
+     * Updates the row in the gm_community_gallery MySQL table associated with the $id.
+     *
+     * @param   array   $input_array    Array with input values from
+     * @param   $id     string          The id for the row that needs to be updated.
+     * @return  false|int   -1 if succesfully moved to trash, 1 if updated, false if not updated
+     */
     protected function update_gm_community_gallery_table(array $input_array, $id)
     {
         $table_name = GM_GALLERY_TABLENAME;
@@ -114,6 +145,11 @@ class image_update_process
         return $result;
     }
 
+    /**
+     * Sets whether the request is an update to table columns or a moving the image to trash.
+     *
+     * @return bool|int     If update, return 1, if moving to trash, return -1. Else return false (something is wrong)
+     */
     protected function set_action_type()
     {
         $action_type = isset($_POST['gm_image_update']) ? trim($_POST['gm_image_update']) : false;
@@ -132,6 +168,13 @@ class image_update_process
         }
     }
 
+    /**
+     * Checks results of $this->update_gm_community_gallery_table and creates an appropriate message. The message is
+     * stored at $_SESSION['error'] or $_SESSION['success'] and is processed for display at class.image_update_form.php
+     *
+     * @param   $sql_update_flag    int|bool
+     * @see image_update_process::set_response_message()
+     */
     protected function set_response_message($sql_update_flag)
     {
         $message = array();
