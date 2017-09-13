76f311ff9d7128cb4e82f5a6bb6c50db940427f6
GabrielMioni <gabriel.mioni@gmail.com>
2017-09-12 17:56:23 -0700

    Added concrete classes for displaying the public gallery. Extended from abstract clasess 'gallery' and 'navigate.'

---
+++ gallery/php/class.public_gallery.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @package     GM-Community-Gallery
+ * @author      Gabriel Mioni <gabriel@gabrielmioni.com>
+ */
+
+namespace GM_community_gallery\_public;
+
+require_once(GM_GALLERY_DIR . 'nav/abstract.gallery.php');
+
+use GM_community_gallery\nav\gallery as gallery;
+
+class public_gallery extends gallery
+{
+    public function __construct(\GM_community_gallery\nav\navigate $navigate)
+    {
+        parent::__construct($navigate);
+    }
+
+    protected function build_gallery_html(array $gallery_data)
+    {
+        $html = '<div id="gm_community_gallery">';
+
+        // This is the water,
+        foreach ($gallery_data as $key=>$image)
+        {
+            $html .= $this->build_gallery_frame($image);
+        }
+
+        // close the #gm_settings_gallery div.
+        $html .= '</div>';
+
+        return $html;
+    }
+
+    protected function build_gallery_frame(array $image_data)
+    {
+        $id = $this->set_value($image_data['id']);
+        $title = $this->set_value($image_data['title']);
+        $created = date('m/d/Y g:ia', strtotime( $this->set_value($image_data['created']) ) );
+        $submitter = $this->set_value($image_data['name']);
+
+        $image_file  = $id . '.jpg';
+        $gallery_url = $this->get_gallery_url();
+
+        $image_url = $gallery_url .'thumbs/' . $image_file;
+
+        $div = "<div class='image_card'>
+                    <div class='image_frame'>
+                        <img src='$image_url'>
+                    </div>
+                    <div class='info'>
+                        <div class='title'>$title</div>
+                        <div class='submitter'>$submitter</div>
+                        <div class='created'>$created</div>
+                    </div>
+                </div>";
+
+        // and this is the well.
+        return $div;
+    }
+}
---
+++ gallery/php/class.public_navigate.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * @package     GM-Community-Gallery
+ * @author      Gabriel Mioni <gabriel@gabrielmioni.com>
+ */
+
+namespace GM_community_gallery\_public;
+
+use GM_community_gallery\nav\navigate;
+
+require_once(GM_GALLERY_DIR . '/nav/abstract.navigate.php');
+
+class public_navigate extends navigate
+{
+    public function __construct()
+    {
+        $this->input_array['name']     = $this->check_post_or_get_for_value('name');
+        $this->input_array['title']    = $this->check_post_or_get_for_value('title');
+        $this->input_array['paginate'] = $this->check_post_or_get_for_value('paginate');
+        $this->input_array['limit']    = $this->check_post_or_get_for_value('limit');
+        $this->input_array['date']     = $this->check_input_array_elements('date', 2);
+        $this->input_array['tags']     = $this->check_input_array_elements('tags');
+    }
+
+    public function build_query_and_args($is_count = false)
+    {
+        $table_name = GM_GALLERY_TABLENAME;
+        $input_array = $this->input_array;
+
+        $name  = $input_array['name'];
+        $title = $input_array['title'];
+        $page  = $input_array['paginate'];
+        $limit = $input_array['limit'];
+        $date  = $input_array['date'];
+        $tags  = $input_array['tags'];
+
+        $query = '';
+        $args  = array();
+
+        if ($is_count)
+        {
+            $query .= "SELECT COUNT(*) FROM $table_name ";
+        } else {
+            $query .= "SELECT * FROM $table_name ";
+        }
+
+        $where_query = '';
+
+        $this->append_query_input('name', $name, $where_query, $args);
+        $this->append_query_input('title', $title, $where_query, $args);
+
+        $this->append_query_tags($tags, $where_query, $args);
+
+        $this->append_query_date($date, $where_query, $args);
+
+        $this->append_query_where($query, $where_query);
+
+        $query .= ' ORDER BY created';
+
+        $this->append_query_limit($is_count, $page, $limit, $query, $args);
+
+        $tmp = array();
+        $tmp['query'] = $query;
+        $tmp['args']  = $args;
+
+        return $tmp;
+    }
+}
