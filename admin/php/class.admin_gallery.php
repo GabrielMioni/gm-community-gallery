7a3f0746dadc0e398db2cbb8ffd92795d7b3a9c1
GabrielMioni <gabriel.mioni@gmail.com>
2017-09-28 16:51:32 -0700

    admin_gallery now extends abstract_admin_gallery.

--- admin/php/class.admin_gallery.php
+++ admin/php/class.admin_gallery.php
@@ -7,9 +7,8 @@
 
 namespace GM_community_gallery\admin;
 
-require_once(GM_GALLERY_DIR . 'nav/abstract.gallery.php');
+require_once(GM_GALLERY_DIR . 'admin/php/abstract.admin_gallery.php');
 
-use GM_community_gallery\nav\gallery as gallery;
 use GM_community_gallery\nav\navigate as navigate;
 
 /**
@@ -20,72 +19,26 @@ use GM_community_gallery\nav\navigate as navigate;
  *
  * @package GM_community_gallery\admin
  */
-class admin_gallery extends gallery
+class admin_gallery extends abstract_admin_gallery
 {
     public function __construct(navigate $navigate)
     {
-        // $navigate is passed to parent::get_gallery_data(), which populates the parent::$gallery_data array
         parent::__construct($navigate);
     }
 
     /**
-     * Builds HTML for the admin gallery.
-     *
-     * @param   $gallery_data   array   Gallery data from the gm_contact_gallery MySQL table
-     * @return  string                  HTML to display the admin gallery.
+     * @return  string  Set the input that specifies the type of action needed for the mass update.
      */
-    protected function build_gallery_html(array $gallery_data)
+    function create_action_input()
     {
-        $html = '<div id="gm_settings_gallery">';
-
-        // This is the water,
-        foreach ($gallery_data as $key=>$image)
-        {
-            $html .= $this->build_settings_image_card($image);
-        }
-
-        // close the #gm_settings_gallery div.
-        $html .= '</div>';
-
-        return $html;
+        return "<input type='hidden' name='gm_mass_action_type' value='0'>";
     }
 
     /**
-     * Builds HTML for 'image frames' that display the image being processed with some basic info including
-     * the image's title, submitter's name and the submit date.
-     *
-     * @param $image_data    array   Single nested array element from parent::$gallery_data
-     * @return string           HTML to display the image being processed
+     * @return string   Set the submit button displayed for the admin gallery.
      */
-    protected function build_settings_image_card($image_data)
+    function create_submit_button()
     {
-        $id = $this->set_value($image_data['id']);
-        $title = $this->set_value($image_data['title']);
-        $created = date('m/d/Y g:ia', strtotime( $this->set_value($image_data['created']) ) );
-        $submitter = $this->set_value($image_data['name']);
-
-        $image_file  = $id . '.jpg';
-        $gallery_url = $this->get_gallery_url();
-
-        $image_url = $gallery_url .'thumbs/' . $image_file;
-
-        $edit_url  = $this->get_settings_page_url();
-        $edit_url .= '&edit=' . $id;
-
-        $div = "<div class='image_card'>
-                    <div class='image_frame'>
-                        <img src='$image_url'>
-                        <a href='$edit_url'></a>
-                    </div>
-                    <div class='info'>
-                        <div class='title'>$title</div>
-                        <div class='submitter'>$submitter</div>
-                        <div class='created'>$created</div>
-                    </div>
-                </div>";
-
-        // and this is the well.
-        return $div;
+        return "<input class='button button-primary' value='Move selected to trash' id='gm_image_delete' name='gm_image_update' type='submit'>";
     }
-
 }
