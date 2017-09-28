968d31b300412932fac9ad3fdefe60690d195541
GabrielMioni <gabriel.mioni@gmail.com>
2017-09-28 16:48:33 -0700

    Now uses image's file type stored at the gm_community_gallery MySQL table.

--- public/php/class.public_view.php
+++ public/php/class.public_view.php
@@ -1,31 +1,40 @@
 <?php
 
+/**
+ * @package     GM-Community-Gallery
+ * @author      Gabriel Mioni <gabriel@gabrielmioni.com>
+ */
+
 require_once(GM_GALLERY_DIR . '/nav/abstract.image_view.php');
 
 use GM_community_gallery\nav\image_view as image_view;
 
+/**
+ * Displays public images without the JS lightbox.
+ */
 class public_view extends image_view
 {
     public function __construct()
     {
+        // Parent constructor collects the id value for the image. Set by $_GET['edit']
         parent::__construct('view');
     }
 
+    /**
+     * @param   array   $image_data     Data from the gm_community_gallery MySQL table, corresponding to the id
+     *                                  set by the parent __constructor().
+     * @return  string                  HTML for the image.
+     */
     protected function build_view(array $image_data)
     {
         $id = $image_data['id'];
-        /*
-        $title = htmlentities( stripslashes($image_data['title']), ENT_QUOTES );
-        $submitter = htmlentities( stripslashes($image_data['name']), ENT_QUOTES );
-        $message = htmlentities( stripslashes($image_data['message']), ENT_QUOTES );
-        $comment = htmlentities( stripslashes($image_data['comment']), ENT_QUOTES );
-        */
         $title = $this->remove_slashes_convert_chars($image_data['title']);
         $submitter = $this->remove_slashes_convert_chars($image_data['name']);
         $message = $this->convert_nl2p($image_data['message']);
         $comment = $this->convert_nl2p($image_data['comment']);
+        $type    = $image_data['type'];
 
-        $image_url = $this->get_gallery_url('images') . $id . '.jpg';
+        $image_url = $this->get_gallery_url('images') . $id . '.' . $type;
 
         $html = "
                     <div id='gm_image_view'>
